e5e62c4fc897d65de94526f9ed0f1e6a68bee6ce
Aliaksandr Palanetski <debrazer@gmail.com>
2014-08-01 23:32:38 +0300

    added html report with screenshots support

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,7 +4,14 @@ Before do
   @browser = Watir::Browser.new :firefox
 end
 
-After do
+After do |scenario|
+  if scenario.failed?
+    file_name = Time.now.to_f.to_s + '.png' # TODO use clear file names
+    @current_page.save_screenshot(FigNewton.framework.results_dir + file_name)
+
+    embed file_name, "image/png"
+  end
+
   @browser.close
 end
 
